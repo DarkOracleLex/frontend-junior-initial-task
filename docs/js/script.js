cdf321cfb83050df34f62e64665b0f58244a9d06
DarkOracleLex <darkoraclelex@yandex.ru>
2020-12-03 14:57:54 +0300

    add pull out menu animation

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,4 @@
+// Для изображений
 function testWebP(callback) {
   var webP = new Image();
   webP.onload = webP.onerror = function () {
@@ -9,8 +10,62 @@ function testWebP(callback) {
 
 testWebP(function (support) {
   if (support == true) {
-    document.querySelector("body").classList.add("webp");
+    document
+      .querySelector("body")
+      .classList.add("webp");
   } else {
-    document.querySelector("body").classList.add("no-webp");
+    document
+      .querySelector("body")
+      .classList.add("no-webp");
   }
 });
+
+// Кнопка с выезжающим меню
+
+const menu = document.querySelector(
+    ".main__pull-out-menu"
+  ),
+  menuButtonUp = document.querySelector(
+    ".main__pull-out-menu-button-up"
+  ),
+  menuButtonDown = document.querySelector(
+    ".main__pull-out-menu-button-down"
+  ),
+  dots = document.querySelector(
+    ".main__dots"
+  );
+
+menuButtonUp.addEventListener(
+  "click",
+  () => {
+    menu.classList.toggle(
+      "main__pull-out-menu--active"
+    );
+    dots.classList.toggle(
+      "visually-hidden"
+    );
+    menuButtonUp.classList.toggle(
+      "visually-hidden"
+    );
+    menuButtonDown.classList.toggle(
+      "visually-hidden"
+    );
+  }
+);
+menuButtonDown.addEventListener(
+  "click",
+  () => {
+    menu.classList.toggle(
+      "main__pull-out-menu--active"
+    );
+    dots.classList.toggle(
+      "visually-hidden"
+    );
+    menuButtonUp.classList.toggle(
+      "visually-hidden"
+    );
+    menuButtonDown.classList.toggle(
+      "visually-hidden"
+    );
+  }
+);
